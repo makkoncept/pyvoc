b7596d8bca8cc35de49a53ba8fe2330f0c358c8d
Mayank Nader <mayank.nader11@gmail.com>
2018-12-30 21:51:50 +0530

    some improvements

--- pyvoc/termoutput.py
+++ pyvoc/termoutput.py
@@ -41,8 +41,10 @@ def revise_vocab(group_number):
 
 
 def validate_group_number(group_number):
+    if group_number == 101:
+        return
     if group_number < 1 or group_number > 100:
-        cprint("Invalid group number", color="red", attrs=["bold"])
+        cprint("Invalid group number. choose from 1-100", color="red", attrs=["bold"])
         exit()
     if group_number < 51:
         path = os.path.join(config_dir_path(), "usergroups.json")
@@ -54,7 +56,7 @@ def validate_group_number(group_number):
 def check_group_path(group_path):
     if not os.path.isfile(group_path):
         cprint(
-            "Dump number {} does not exist".format(group_number),
+            "group number {} does not exist".format(group_number),
             color="red",
             attrs=["bold"],
         )
@@ -81,27 +83,40 @@ def quiz(group_number, no_of_questions=5):
     group_path = os.path.join(config_dir_path(), "group" + str(group_number) + ".json")
     options_path = os.path.join(config_dir_path(), "definitions.json")
     check_group_path(group_path)
-    count_words_in_group(path, group_number, no_of_questions)
+    if path:
+        count_words_in_group(path, group_number, no_of_questions)
     result = {}
     word_definition = {}
     with open(group_path, "r") as f:
         group_content = json.load(f)
     word_list = random.sample(list(group_content), no_of_questions)
-    print(word_list)
+    # print(word_list)
     for word in word_list:
         _ = group_content[word]
         refined_def = _[random.sample(list(_), 1)[0]]
-        print(_)
-        print(refined_def)
         word_definition[word] = refined_def
-    print(word_definition)
     with open(options_path, "r") as f:
         options = json.load(f)
+    cprint("\t\tStarting Quiz", color="red", attrs=["bold", "reverse"])
+    cprint(
+        "1 point for every correct answer. q<enter> to exit",
+        color="yellow",
+        attrs=["bold"],
+    )
+    print("\n")
+    score = 0
     for i in range(no_of_questions):
-        cprint("{}- {}:".format(i + 1, word_list[i]), color="yellow")
+        # print("{} ".format(i + 1), end="")
+        cprint(word_list[i], color="white", attrs=["bold", "reverse"])
         correct_option_number = print_options(options, word_definition, word_list[i])
-        prompt_input(result, correct_option_number, word_list[i])
-    print(result)
+        prompt_input(correct_option_number, word_list[i], score, result)
+    for word in result:
+        if result[word] is True:
+            score += 1
+    print("")
+    cprint("Score: {}/{}".format(score, no_of_questions), color="green", attrs=["bold"])
+    if score == no_of_questions:
+        cprint("Perfect Score ヽ(´▽`)/", color="yellow", attrs=["bold", "blink"])
 
 
 def print_options(options, correct_answer, word):
@@ -112,10 +127,11 @@ def print_options(options, correct_answer, word):
     options_list.append(correct_answer[word])
     random.shuffle(options_list)
     count = 1
-    for option in options_list:
+    for i, option in enumerate(options_list, 1):
         if option == correct_answer[word]:
             correct_option_number = count
-        cprint(option, color="red")
+        cprint("[{}] ".format(i), color="cyan", end="")
+        cprint(option)
         count += 1
     return correct_option_number
 
@@ -125,30 +141,32 @@ def print_options(options, correct_answer, word):
 
 
 # todo: print the score even if user exit the quiz instead of terminating.
-def prompt_input(result, correct_option_number, word):
+def prompt_input(correct_option_number, word, score, result):
     while 1:
         prompt = input("> ")
-        print(type(prompt))
         if prompt.lower() == "q":
-            exit()
+            return
+            # exit()
         try:
             if not int(prompt) in [1, 2, 3, 4]:
                 cprint(
-                    "enter a valid integer[1, 2, 3, 4]. q<enter> to exit", color="cyan"
+                    "enter a valid integer[1, 2, 3, 4]. q<enter> to exit",
+                    color="yellow",
                 )
                 continue
         except ValueError:
-            cprint("enter a valid integer[1, 2, 3, 4]. q<enter> to exit", color="cyan")
+            cprint(
+                "enter a valid integer[1, 2, 3, 4]. q<enter> to exit", color="yellow"
+            )
             continue
         if int(prompt) == correct_option_number:
-            cprint(
-                "correct anwer",
-                color="yellow",
-                on_color="on_grey",
-                attrs=["bold", "blink"],
-            )
+            cprint("correct anwer", color="green", on_color="on_grey")
+            print("")
             result[word] = True
+            score += 1
             break
         else:
+            cprint("wrong answer", color="red", on_color="on_grey")
+            print("")
             result[word] = False
             break
