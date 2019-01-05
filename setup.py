afe2777347055c61873bbb6bfef5f292f89afd8b
Mayank Nader <mayank.nader11@gmail.com>
2019-01-06 00:42:00 +0530

    update requirements in setup.py

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ from setuptools import setup
 
 
 setup(
-    name='pyvoc',
-    version='0.10',
-    description='dictionary cum vocabulary manager',
-    author='Mayank Nader',
-    author_mail='nader.mayank@gmail.com',
-    packages=['pyvoc'],
-    install_requires=['requests'],
+    name="pyvoc",
+    version="0.10",
+    description="dictionary cum vocabulary manager",
+    author="Mayank Nader",
+    author_mail="nader.mayank@gmail.com",
+    packages=["pyvoc"],
+    install_requires=["requests", "termcolor", "colorama"],
     entry_points={"console_scripts": ["pyvoc=pyvoc.pyvoc:main"]},
 )
