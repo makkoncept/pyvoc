53fb09b85e4686bfad80a46437599b8a8953e158
Mayank Nader <mayank.nader11@gmail.com>
2019-01-06 19:19:24 +0530

    update setup.py

--- setup.py
+++ setup.py
@@ -1,14 +1,22 @@
 from setuptools import setup
 
 
+with open("README.md") as infile:
+    long_description = infile.read()
+
+
 setup(
     name="pyvoc",
     version="0.10",
+    packages=["pyvoc"],
     description="cross-platform dictionary and vocabulary building command line tool.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    license="MIT",
     url="http://github.com/makkoncept/pyvoc",
     author="Mayank Nader",
     author_mail="nader.mayank@gmail.com",
-    packages=["pyvoc"],
     install_requires=["requests", "termcolor", "colorama"],
     entry_points={"console_scripts": ["pyvoc=pyvoc.pyvoc:main"]},
+    include_package_data=True,
 )
